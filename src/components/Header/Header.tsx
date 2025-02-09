7e5c58d156dce6af6c5666070eda7d2d51bfc450
Ivan <statkevich.ivan999@gmail.com>
2025-02-09 09:59:00 +0200

    Add Header component with styling, update layout grid, and enhance global CSS variables

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,10 @@
 import ThemeToggler from "@/components/ThemeToggler/ThemeToggler";
 import Logo from "@/components/Logo/Logo";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <header className="">
+    <header className={styles.header}>
       <Logo />
       <nav className=""></nav>
       <ThemeToggler />
