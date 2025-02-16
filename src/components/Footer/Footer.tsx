f2fb09367bf64a5cd4cbd3fd60687f5cbbbadbd0
Ivan <statkevich.ivan999@gmail.com>
2025-02-16 10:38:36 +0200

    Update Footer component styles and add author link for improved layout and attribution

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,6 +6,17 @@ export default function Footer() {
       <p className={styes.copyright}>
         © {new Date().getFullYear()} Sentiment Atlas. All rights reserved.
       </p>
+      <p className={styes.author}>
+        Author:{" "}
+        <a
+          className={styes.link}
+          rel="noreferrer noopener"
+          target="_blank"
+          href="https://www.linkedin.com/in/ivan-statkevich-a9868819b/"
+        >
+          Ivan Statkevich
+        </a>
+      </p>
     </footer>
   );
 }
